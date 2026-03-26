8bd060bc4d82f36cd1245f8384cda27a2ee7921a
sample123@gmail.com <sample@gmail.com>
2026-03-26 11:46:31 +0530

    changed from .NET Framework to Console App

---
+++ Bank System/Program.cs
@@ -0,0 +1,39 @@
+﻿using Bank_System.Exceptions;
+
+namespace Bank_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter User Name : ");
+                string name = Console.ReadLine();
+
+                if (name == "")
+                {
+                    throw new InvalidInput("name cannot be null or empty");
+                }
+
+                Console.Write("Enter Account No. : ");
+                long acc_no = Convert.ToInt64(Console.ReadLine());
+
+                if (acc_no <= 0)
+                {
+                    throw new InvalidInput("Account Number should be greater than 0");
+                }
+                Console.WriteLine();
+
+                Customer_Account acc = new Customer_Account(acc_no, name) { bank_name = "ICICI Bank" };
+
+                // Display Info.
+                acc.PrintInfo();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
